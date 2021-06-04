389bb4439874fc7d0d80aab8847eda6262831507
Priyanshu-Desai <desai09p@gmail.com>
2021-06-04 15:28:14 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# weather-site
+a simple website that shows the weather for any location using node.js and an api
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const http = require('http')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/', function(req, res) {
+  const url = 'http://api.openweathermap.org/data/2.5/weather?q='+req.body.postcodeSearchBox+'&appid=346eda849bac45f08391170e473a0f11&units=metric'
+  http.get(url, function(response){
+    console.log(response.statusCode)
+    response.on('data', function(data){
+      const weather = JSON.parse(data)
+      const description = weather.weather[0].description
+      const icon = weather.weather[0].icon
+      const temp = weather.main.temp
+
+      res.write('<h1>'+req.body.postcodeSearchBox+'</h1>')
+      res.write('<h2>'+description+'</h2>')
+      res.write('<img src= http://openweathermap.org/img/wn/'+icon+'@2x.png >')
+      res.write('<p>temperature: '+temp+' degrees celcius</p>')
+      res.send()
+    })
+  })
+})
+
+app.listen(5000, function() {
+  console.log('server running on port 5000')
+})
